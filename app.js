b0177d0d1c4c4eb2f024e8623e6e62a43717f40d
AlexJohnDev <alexjohnweb@outlook.com>
2023-09-10 17:46:52 +0100

    Logic applied to merge data called from API to generated pagination and distribute that data between the pages. Now displaying a set amount of items per page and functionalities to use all navigational controls in pagination are operational

--- app.js
+++ app.js
@@ -1,35 +1,47 @@
 $(document).ready(function () {
-  // Function to fetch data and populate the table
-  // This can be in a separate file or embedded here
+  // Pagination plugin settings
 
   $("#pagination").twbsPagination({
     totalPages: 10,
     visiblePages: 5,
     next: "Next",
     prev: "Prev",
-    onPageClick: function (event, page) {},
-  });
+    onPageClick: function (event, page) {
+      //Variables
+      let $tableData = $("#dataTable");
+      let $userIdentification = 1;
 
-  let $tableData = $("#dataTable");
-  $.ajax({
-    url: "https://jsonplaceholder.typicode.com/todos",
-    type: "GET",
-    success: function (tableData) {
-      $.each(tableData, function (i, item) {
-        $tableData.append(
-          "<tr>" +
-            "<td>" +
-            item.id +
-            "</td>" +
-            "<td>" +
-            item.title +
-            "</td>" +
-            "<td>" +
-            item.completed +
-            "</td>" +
-            "</tr>"
-        );
-      });
+      // Call to API via AJAX
+      if (($userIdentification = page)) {
+        $("#dataTable tbody").text([$userIdentification[0]]);
+        $.ajax({
+          url: "https://jsonplaceholder.typicode.com/todos",
+          type: "GET",
+          data: {
+            userId: $userIdentification,
+          },
+          success: function (tableData) {
+            $.each(tableData, function (i, item) {
+              $tableData.append(
+                "<tr>" +
+                  "<td>" +
+                  item.id +
+                  "</td>" +
+                  "<td>" +
+                  item.title +
+                  "</td>" +
+                  "<td>" +
+                  item.completed +
+                  "</td>" +
+                  "</tr>"
+              );
+            });
+          },
+        });
+      }
     },
   });
+
+  // Function to fetch data and populate the table
+  // This can be in a separate file or embedded here
 });

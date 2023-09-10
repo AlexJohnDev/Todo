d1bbd0561c3307db8a43ed9c692763e7b39a6d44
AlexJohnDev <alexjohnweb@outlook.com>
2023-09-10 18:38:51 +0100

    Added functionality to the search bar to allow search filtering to operate

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 $(document).ready(function () {
   // Pagination plugin settings
-
   $("#pagination").twbsPagination({
     totalPages: 10,
     visiblePages: 5,
@@ -11,9 +10,10 @@ $(document).ready(function () {
       let $tableData = $("#dataTable");
       let $userIdentification = 1;
 
-      // Call to API via AJAX
+      // Linking data's userID to corresponding page in pagination
       if (($userIdentification = page)) {
         $("#dataTable tbody").text([$userIdentification[0]]);
+        // Call to API via AJAX
         $.ajax({
           url: "https://jsonplaceholder.typicode.com/todos",
           type: "GET",
@@ -42,6 +42,11 @@ $(document).ready(function () {
     },
   });
 
-  // Function to fetch data and populate the table
-  // This can be in a separate file or embedded here
+  //Search funtionality
+  $("#myInput").on("keyup", function () {
+    var value = $(this).val().toLowerCase();
+    $("#dataTable tbody tr").filter(function () {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
 });
